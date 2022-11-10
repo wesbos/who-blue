fca705c18a370b95353547caa54efe4216046b25
Wes Bos <wesbos@gmail.com>
2022-11-10 10:12:16 -0500

    i blue it

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Who Blue?
+
+This is a joke so please relax. Just showing some cool coding techniques.
+
+A browser extension to separate the lords from the peasants. Turns $8 blue checks into upsidedown pink ones.
+
+![](./images/josh-is-cool.png)
---
+++ scripts/content.js
@@ -0,0 +1,57 @@
+const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+
+async function isBlue() {
+  const checkmark = document.querySelector(`[aria-label*="verified accounts"]`);
+
+  if (!checkmark) return false;
+
+  // click it
+  checkmark.click();
+  // wait for a bit
+  await wait(50);
+  const blueText = Array.from(document.querySelectorAll('span')).find(span => span.innerText.includes('subscribed to Twitter Blue'));
+
+  checkmark.click();
+
+  if (blueText) return true;
+  return false;
+}
+
+function markAsBlue() {
+  const check = document.querySelector(`[aria-label*="verified accounts"] svg`);
+  const otherCheck = document.querySelector(`svg[aria-label="Verified account"]`)
+
+  if (!check) return;
+  [check, otherCheck].filter(check => check.style).forEach(check => {
+    check.style.rotate = `0.5turn`;
+    check.style.fill = `#ee8383`;
+  });
+}
+
+async function go() {
+  await waitForTimeline();
+  console.log('checking if blue')
+  await wait(500);
+  const isBlueCheck = await isBlue();
+  if (isBlueCheck) {
+    console.log('IS BLUE')
+    markAsBlue();
+  }
+}
+
+async function waitForTimeline() {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(function() {
+      console.log('checking for timeline...');
+      const timeline = document.querySelector(`[aria-label="Home timeline"]`);
+
+      if(timeline){
+        console.log('IT WORKED')
+        clearInterval(interval);
+        resolve();
+      }
+    }, 50);
+  });
+}
+
+go();
